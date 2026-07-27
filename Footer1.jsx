3a27fd001c30385ed21a152c289c7bf128f89673
Nagender <nk4397063@gmail.com>
2026-07-27 13:09:36 +0530

    Change brand name in Footer1 component
    Updated the brand name from 'Verdal' to 'Sri Laxmi Furnitures' in the footer component.

---
+++ Footer1.jsx
@@ -0,0 +1,3 @@
+Updating the brand name to "Sri Laxmi Furnitures" across the site:
+Action: search_replace({"new_str":"          <div className=\"font-serif text-3xl\">Sri Laxmi<span className=\"text-[#9E643A]\">.</span></div>","old_str":"          <div className=\"font-serif text-3xl\">Verdal<span className=\"text-[#9E643A]\">.</span></div>","path":"/app/frontend/src/components/site/Footer.jsx"})
+Observation: Edit was successful.
