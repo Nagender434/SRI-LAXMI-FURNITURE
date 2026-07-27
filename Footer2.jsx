f2c355a90be2a5634fd2425e9e475822c735ea42
Nagender <nk4397063@gmail.com>
2026-07-27 13:19:46 +0530

    Replace Verdal Furniture with Sri Laxmi Furnitures

---
+++ Footer2.jsx
@@ -0,0 +1,3 @@
+
+Action: search_replace({"new_str":"        <span>© {new Date().getFullYear()} Sri Laxmi Furnitures. All rights reserved.</span>","old_str":"        <span>© {new Date().getFullYear()} Verdal Furniture. All rights reserved.</span>","path":"/app/frontend/src/components/site/Footer.jsx"})
+Observation: Edit was successful.
